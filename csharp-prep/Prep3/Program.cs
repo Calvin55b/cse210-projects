587128456602066188759b6f7079b3edd7739721
Calvin55b <rop22006@byui.edu>
2025-09-24 10:23:30 -0600

    I made a magic number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        
+        
+
+        Random randomGenerator = new Random();
+        int ranNumber = randomGenerator.Next(1, 11);
+
+        int usersGuess = -1;
+
+        
+        while (usersGuess != ranNumber)
+        {
+            Console.Write("What is your guess? ");
+            usersGuess = int.Parse(Console.ReadLine());
+    
+
+            if (usersGuess < ranNumber)
+            {
+                Console.WriteLine("Higher");
+                
+
+            }
+
+            else if (usersGuess > ranNumber)
+            {
+                Console.WriteLine("Lower");
+               
+            }
+
+            else if (usersGuess == ranNumber)
+            {
+
+                Console.WriteLine("You guessed the correct number! ");
+                
+            }
+            
+        }
     }
+
+    
 }
