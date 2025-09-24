3feac22082c3662581a821be93047ec262fa16c4
Calvin55b <rop22006@byui.edu>
2025-09-24 17:56:23 -0600

    I did the prep 3 and 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userNum = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> nums = new List<int>();
+
+        while (userNum != 0)
+
+        {
+            Console.Write("Enter number: ");
+            userNum = int.Parse(Console.ReadLine());
+
+            if (userNum != 0)
+            {
+                nums.Add(userNum);
+
+            }
+        }
+        int greatest = nums[0];
+
+        foreach (int number in nums)
+        {
+            if (number > greatest)
+            {
+                greatest = number;
+            }
+
+        }
+
+        int sum = 0;
+        foreach (int number in nums)
+        {
+            sum += number;
+        }
+
+
+
+        float average = (float)sum / nums.Count();
+
+
+        Console.WriteLine($"The Sum is: {sum} ");
+        Console.WriteLine($"The average is: {average} ");
+        Console.WriteLine($"The largest number is: {greatest}");
     }
 }
