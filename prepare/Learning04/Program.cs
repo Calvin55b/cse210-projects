5fbeab8a11303cb12d83471648b1a6ae86384721
Calvin55b <rop22006@byui.edu>
2025-10-29 12:13:42 -0600

    I was able to complete all of my parent and child classes!!!

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program
+public class Program1
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("John", "Wayne");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Rodrigo Calsario", "Multiplication", "Section 7.5", "Problems 15-19");
+
+        Console.WriteLine(mathAssignment1.GetSummary());
+
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Juan", "Idaho History", "The death of Napoleon Dynamite");
+
+
+        Console.WriteLine(writingAssignment1.GetSummary());
+
+
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+
+
+
+
+
+        
+
+        
+
+       
+
     }
 }
