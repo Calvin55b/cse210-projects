6444d9e37dc7edf9a686775ce60437482e937476
Calvin55b <rop22006@byui.edu>
2025-11-11 20:26:31 -0700

    I was able to create my program!

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,29 @@
+using System.Net.NetworkInformation;
+
+public class Circle : Shape
+{
+
+
+    private double _radius;
+
+public Circle(string color, double radius): base(color)
+    {
+        _radius = radius;
+
+    
+    }
+
+
+
+    public override double GetArea()
+    {
+
+        return _radius * 2 * 3.14;
+    }
+
+
+
+
+
+
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>() { };
+
+        Rectangle rectangle = new Rectangle("blue", 6, 7);
+       
+
+        Console.WriteLine($"The color of the rectangle is {rectangle.GetColor()} and it has an area of {rectangle.GetArea()}");
+
+        Square square = new Square("red",6);
+       
+
+        Console.WriteLine($"The squares color is {square.GetColor()} and it has an area of {square.GetArea()}");
+
+        Circle circle = new Circle("white",6);
+
+
+        Console.WriteLine($"The squares color is {circle.GetColor()} and it has an area of {circle.GetArea()}");
+
+        Console.WriteLine();
+
+        shapes.Add(rectangle);
+        shapes.Add(square);
+        shapes.Add(circle);
+
+        foreach(Shape s in shapes)
+        {
+
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+
+            Console.WriteLine($"color: {color}\n area: {area}");
+
+        }
     }
 }
