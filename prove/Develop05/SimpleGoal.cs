fc185a6644b369a6575aa8d73314687419e72741
Calvin55b <rop22006@byui.edu>
2025-11-17 11:34:45 -0700

    I started adding my constructors and attributes

---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,73 @@
+public class SimpleGoal
+{
+    //Attributes//
+
+
+
+    private string _name;
+
+    private string _description;
+
+
+    private int _points;
+
+
+    private int _totalPoints;
+
+
+    private bool _done;
+
+
+
+
+    // Constructors//
+
+    // This gets the / stores the users input for the name of their goal and description of that goal.
+    public SimpleGoal(string name, string description)
+    {
+        
+        _name = name;
+
+        _description = description;
+
+
+    }
+
+    // adds the points from each activity to the cummalitive score.
+    public void GetCumulativeScore()
+    {
+        int totalPoints = 0;
+        _totalPoints = totalPoints;
+
+    }
+
+    // points for each completed activity
+    public void Points(int Points)
+    {
+        
+        _points = Points;
+
+    }
+
+    public void Display ()
+    {
+        
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
